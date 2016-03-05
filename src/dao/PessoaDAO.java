578662e3d5834eb5ac153d1ae05300df78559491
jonatas luis <jonatasluis.unipe@gmail.com>
2016-03-05 13:13:03 -0300

    new commit

--- src/dao/PessoaDAO.java
+++ src/dao/PessoaDAO.java
@@ -16,44 +16,30 @@ public class PessoaDAO {
 	}
 
 	public void inserir(Pessoa pessoa) {
-
 		this.manager.getTransaction().begin();
-
 		this.manager.persist(pessoa);
-
 		this.manager.getTransaction().commit();
-
 	}
 
 	public void atualizar(Pessoa pessoa) {
-
 		this.manager.getTransaction().begin();
-
 		this.manager.merge(pessoa);
-
 		this.manager.getTransaction().commit();
-
 	}
 
 	public void remover(Long id) {
-
 		this.manager.getTransaction().begin();
 		Pessoa p = buscar(id);
 		this.manager.remove(p);
 		this.manager.getTransaction().commit();
-
 	}
 
 	private Pessoa buscar(Long id) {
-
 		return this.manager.find(Pessoa.class, id);
-
 	}
 
 	public List<Pessoa> buscarTodos() {
-
 		return manager.createQuery("SELECT p from Pessoa p", Pessoa.class).getResultList();
-
 	}
 
 }
